3746b68e9c4c10a7ca332a573d128fdaf27af89f
hoparkgo9ma <me@ho9.me>
2018-03-03 21:28:34 +0900

    update app.py
    배그하니까 세부 검색은 나중에

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#-*- coding:utf-8 -*-
+
 import json
 import sys
 #PyQt must be installed.
@@ -6,11 +8,9 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-#class word:
-#    shortNative = open('data/short.json').read()
-#    longNative = open('data/long.json').read()
-#    shortJson = json.loads(shortNative)
-#    longJson = json.loads(longNative)
+longNative = open('data/long.json', encoding = 'utf-8').read()
+print(type(longNative))
+longJson = json.loads(longNative)
 
 formClass = uic.loadUiType('ui.ui')[0]
 
@@ -20,6 +20,9 @@ with open('data/long.txt', mode='r', encoding='utf-8') as file:
         lines[0] = lines[0].replace(u"\ufeff", '')
 longdata = [s.strip('\n') for s in lines]
 
+Culture = True
+Manner = True
+
 class MainformClass(QMainWindow, formClass):
     def __init__(self):
         super().__init__()
@@ -27,12 +30,26 @@ class MainformClass(QMainWindow, formClass):
         self.wordInput.textChanged.connect(self.wordSearch)
 
     def wordSearch(self):
+        self.setCulture()
+        self.setManner()
         searchTarget = self.wordInput.text()
         self.listWidget.clear()
         if not searchTarget == '':
             for i in range(len(longdata)):
                 if searchTarget[0] == longdata[i][0]:
                     self.listWidget.addItem(longdata[i])
+    
+    def setCulture(self):
+        if self.CheckBoxCulture.isChecked():
+            Culture = True
+        else:
+            Culture = False
+
+    def setManner(self):
+        if self.CheckBoxMannerEnable.isChecked():
+            Manner = True
+        else:
+            Manner = False
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
