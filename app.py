da3dae81c89e8764dcc668fe04595bf6dd830aa9
hoparkgo9ma <me@ho9.me>
2018-03-04 10:47:57 +0900

    update app.py
    세부검색 기능 활성화

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-longNative = open('data/long.json', encoding = 'utf-8').read()
-print(type(longNative))
-longJson = json.loads(longNative)
-
 formClass = uic.loadUiType('ui.ui')[0]
+
+longdata = []
+
+
+longNative = open('data/long.json', encoding = 'utf-8').read()
+longJson = json.loads(longNative)
 
 with open('data/long.txt', mode='r', encoding='utf-8') as file:
     lines = file.readlines()
@@ -20,8 +22,6 @@ with open('data/long.txt', mode='r', encoding='utf-8') as file:
         lines[0] = lines[0].replace(u"\ufeff", '')
 longdata = [s.strip('\n') for s in lines]
 
-Culture = True
-Manner = True
 
 class MainformClass(QMainWindow, formClass):
     def __init__(self):
@@ -30,26 +30,30 @@ class MainformClass(QMainWindow, formClass):
         self.wordInput.textChanged.connect(self.wordSearch)
 
     def wordSearch(self):
-        self.setCulture()
-        self.setManner()
+        culture = self.setCulture()
+        manner = self.setManner()
         searchTarget = self.wordInput.text()
         self.listWidget.clear()
         if not searchTarget == '':
             for i in range(len(longdata)):
                 if searchTarget[0] == longdata[i][0]:
-                    self.listWidget.addItem(longdata[i])
+                    if culture == True and str(manner) == longJson[longdata[i]]['onepunch']:
+                        self.listWidget.addItem(longdata[i])
+                    elif culture == False and str(manner) == longJson[longdata[i]]['onepunch']:
+                        if longJson[longdata[i]]['culture'] == 'False':
+                            self.listWidget.addItem(longdata[i])
     
     def setCulture(self):
         if self.CheckBoxCulture.isChecked():
-            Culture = True
+            return True
         else:
-            Culture = False
+            return False
 
-    def setManner(self):
+    def setManner(self): #원 펀치 여부 반환
         if self.CheckBoxMannerEnable.isChecked():
-            Manner = True
+            return False
         else:
-            Manner = False
+            return True
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
